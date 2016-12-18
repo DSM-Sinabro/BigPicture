38a87b227053f61df49ca4121f00816311da0912
jeong-minji <alswl4152@naver.com>
2016-12-18 22:48:40 +0900

    doingnews

--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@ $(function () {
         },500);
     });
 });
+
+$(document).ready(function(){
+    $("#btn_apply").click(function(){
+        $("#myModal_apply" ).modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn_suggest").click(function(){
+        $("#myModal_suggest" ).modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn_event").click(function(){
+        $("#myModal_event" ).modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn_news").click(function(){
+        $("#myModal_news" ).modal();
+    });
+});
