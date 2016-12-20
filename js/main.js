3dab1d15c6aa595972e3aa03ecabd171eb3740f1
jeong-minji <alswl4152@naver.com>
2016-12-20 17:40:31 +0900

    new

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-$(function () {
-    $("#btn_start").on("click",function () {
+$(function() {
+    $("#btn_start").on("click", function() {
         $("html, body").animate({
-            scrollTop : $("#second").offset().top
-        },500);
+            scrollTop: $("#second").offset().top
+        }, 500);
     });
 });
 
-$(document).ready(function(){
-    $("#btn_apply").click(function(){
-        $("#myModal_apply" ).modal();
+$(document).ready(function() {
+    $("#btn_apply").click(function() {
+        $("#myModal_apply").modal();
     });
 });
 
-$(document).ready(function(){
-    $("#btn_suggest").click(function(){
-        $("#myModal_suggest" ).modal();
+$(document).ready(function() {
+    $("#btn_suggest").click(function() {
+        $("#myModal_suggest").modal();
     });
 });
 
-$(document).ready(function(){
-    $("#btn_event").click(function(){
-        $("#myModal_event" ).modal();
+$(document).ready(function() {
+    $("#btn_event").click(function() {
+        $("#myModal_event").modal();
     });
 });
 
-$(document).ready(function(){
-    $("#btn_news").click(function(){
-        $("#myModal_news" ).modal();
+$(document).ready(function() {
+    $("#btn_news").click(function() {
+        $("#myModal_news").modal();
     });
 });
