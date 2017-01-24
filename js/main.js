3ea37e539986b2d0d8b8ce62f00029ad4c222ccb
unknown <>
2017-01-24 15:28:03 +0900

    test

--- js/main.js
+++ js/main.js
@@ -10,21 +10,15 @@ $(document).ready(function() {
     $("#btn_apply").click(function() {
         $("#myModal_apply").modal();
     });
-});
 
-$(document).ready(function() {
     $("#btn_suggest").click(function() {
         $("#myModal_suggest").modal();
     });
-});
 
-$(document).ready(function() {
     $("#btn_event").click(function() {
         $("#myModal_event").modal();
     });
-});
 
-$(document).ready(function() {
     $("#btn_news").click(function() {
         $("#myModal_news").modal();
     });
